76aa329c7ede7069e3e3e691f5c1a0d72dc6a514
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-12-18 20:19:18 +0300

    First Commit 2.0

---
+++ controllers/tracks.js
@@ -0,0 +1,5 @@
+const Track = require("../models/tracks.js")
+const express = require("express")
+const router = express.Router()
+
+module.export = router
--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongoose = require("mongoose")
 
+const tracksRouter = require("./controllers/tracks.js")
+
 mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+
+app.use(express.json())
+
+app.use("/tracks", tracksRouter)
+
+app.listen(3000, () => {
+  console.log("The express app is ready!")
 })
