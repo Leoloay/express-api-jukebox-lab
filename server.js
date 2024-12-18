8034c421f2a2f32048d5a3dfc6027b0918042004
Loay Al-Mugharghar <almughargharloay@gmail.com>
2024-12-18 20:19:04 +0300

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+
+dotenv.config()
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
